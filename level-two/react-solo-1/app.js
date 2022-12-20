072f5ca8f7acac09aa6f7dbc3b09457af769fdb0
Aloe Nelson <aloe.eon@gmail.com>
2022-12-19 21:10:29 -0800

    updated styles

---
+++ level-two/react-solo-1/app.js
@@ -0,0 +1,14 @@
+import React from "react"
+import Info from "/components/info.js"
+import Main from "/components/main.js"
+import Footer from "/components/footer.js"
+
+export default function App() {
+    return (
+        <div>
+            <Info />
+            <Main />
+            <Footer />
+        </div>
+    )
+}
---
+++ level-two/react-solo-1/components/info.js
@@ -0,0 +1,12 @@
+import React from "react"
+
+export default function Info() {
+    return (
+        <div>
+            <h2 className='name'>Laura Smith</h2>
+            <h3 className='title'>Frontend Developer</h3>
+            <p className='website'>laurasmith.website</p>
+            <button className="email"><i className="fa-solid fa-envelope"></i>Email</button>
+        </div>
+    )
+}
